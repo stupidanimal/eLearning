936e2ac5d2f46bde7adf7e309cb17ac47159f3c4
seriousSam <stupidanimal@outlook.com>
2018-04-20 08:49:40 +0800

    添加orgnazition中的相关内容

--- orgnazition/admin.py
+++ orgnazition/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher,CityDict,CourseOrg
+
+admin.site.register(Teacher)
+admin.site.register(CityDict)
+admin.site.register(CourseOrg)
