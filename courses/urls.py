7d029f9eca2351c6883fd09d14f0f59fc05b496a
evaseemefly-aw <evaseemefly@126.com>
2018-04-22 21:43:17 +0800

    完成部分修改

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 
-from .views import CoursesListView
+from .views import CoursesListView,CoursesIndexView
 
 
 app_name='[courses]'
 urlpatterns=[
+    url(r'^home/$',CoursesIndexView.as_view(),name="courses_home"),
     url(r'^list/$',CoursesListView.as_view(),name="courses_list")
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,23 @@
+from django.db.models import Q
+
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Course
 # Create your views here.
+
+class CoursesIndexView(View):
+    def get(self,request):
+        '''
+        获取全部的课程信息
+        :param request:
+        :return:
+        '''
+        # list_all_courses=Course.objects.all()
+        return render(request,'courses_list.html',{
+            # "allcourses":list_all_courses
+        })
 
 class CoursesListView(View):
     def get(self,request):
@@ -9,6 +26,11 @@ class CoursesListView(View):
         :param request:
         :return:
         '''
-        return render(request,'courses_list.html',{
-
-        })
+        # 1 获取全部的课程集合
+        list_all_courses=Course.objects.all()
+        # 2 序列化
+        json_courses=serializers.serialize(list_all_courses)
+        return JsonResponse({'allcourses':json_courses})
+        # return render(request,'courses_list.html',{
+        #     "allcourses":list_all_courses
+        # })
